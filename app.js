eee0906ed3f7e3f46ab5aa7820f012a4e2f205f9
apingut <apingut@gmail.com>
2015-07-13 23:52:42 +0200

    Con los cambios solicitados

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials'):
-
 var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -15,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(partials());
+
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
