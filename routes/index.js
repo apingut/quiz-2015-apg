c2a39f2248b303f61989e6d1f7031964a26314b6
apingut <apingut@gmail.com>
2015-07-21 06:48:47 +0100

    Con los cambios solicitados

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ router.get('/', function(req, res) {res.render('index', { title: 'Quiz' });});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-router.get('/author', function(req,res) {res.render('author', { title: 'Autor'});});
+router.get('/quizes/author', quizController.author);
 
 module.exports = router;
